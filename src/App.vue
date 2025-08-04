cc9322a59107145a0407c9c1cbf19b1f60e7dd8b
minsun24 <jms49526@gmail.com>
2025-08-04 13:59:11 +0900

    Init Vue project on vue-v branch

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AppHeader from './common/components/AppHeader.vue';
+
+
+
+</script>
+
+<template>
+  <AppHeader />
+  <v-main>
+    <router-view />
+  </v-main>
+  <h1>chat client</h1>
+</template>
+
+<style scoped></style>
